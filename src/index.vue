476174a33fc75f5649b4f0c4f5253b448af8efce
zhanshichao <zhanshichao@dianwoba.com>
2018-03-04 11:30:16 +0800

    eslint

--- src/index.vue
+++ src/index.vue
@@ -7,14 +7,14 @@
 </template>
 
 <script>
-  export default {
-    name: 'App',
-    data () {
-      return {
-        logo: 'https://gw.alicdn.com/tfs/TB1yopEdgoQMeJjy1XaXXcSsFXa-640-302.png',
-      }
+export default {
+  name: 'App',
+  data () {
+    return {
+      logo: 'https://gw.alicdn.com/tfs/TB1yopEdgoQMeJjy1XaXXcSsFXa-640-302.png'
     }
   }
+}
 </script>
 
 <style scoped>
